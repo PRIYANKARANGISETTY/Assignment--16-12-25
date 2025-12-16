9b9770542d9703c0f8bb1dede4e919d12a615f15
PRIYANKARANGISETTY <161281072+PRIYANKARANGISETTY@users.noreply.github.com>
2025-12-16 21:58:29 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div style={{ fontSize: "14px", color: "gray", textAlign: "center" }}>
+      <p>This is Footer</p>
+    </div>
+  );
+}
+
+export default Footer;
